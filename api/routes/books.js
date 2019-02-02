e828e6fe1c9c3d272d0ef6c82137eb2f73d61e45
Róbert Székely <szekelyrobert1995@gmail.com>
2019-02-03 01:05:20 +0200

    Handle more routes with ids

--- api/routes/books.js
+++ api/routes/books.js
@@ -13,4 +13,30 @@ router.post('/', (req, res, next) => {
     });
 });
 
+router.get('/:bookId', (req, res, next) => {
+    const id = req.params.bookId;
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'You discovered the special ID',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        });
+    }
+});
+
+router.patch('/:bookId', (req, res, next) => {
+   res.status(200).json({
+       message: 'Updated book'
+   })
+});
+
+router.delete('/:bookId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted book'
+    })
+ });
+
 module.exports = router;
