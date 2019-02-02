5f2dfcb17b9cd46b6f87a4911c5a47e96aab1580
Róbert Székely <szekelyrobert1995@gmail.com>
2019-02-03 00:56:41 +0200

    Handle books get and post requests with a message

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'It works!'
-    });
-})
+const bookRoutes = require('./api/routes/books');
+
+app.use('/books', bookRoutes);
 
 module.exports = app;
