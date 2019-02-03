3cdbc5bdd9432ba17afe7a174717a3e74560d84d
Róbert Székely <szekelyrobert1995@gmail.com>
2019-02-03 23:52:38 +0200

    Sign and return token for successful login

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -59,9 +60,20 @@ router.post('/login', (req, res, next) => {
                         message: 'Email or password incorrect.'
                     });
                 }
-                if(result) {
+                if (result) {
+                    const token = jwt.sign(
+                        {
+                            email: user[0].email,
+                            userId: user[0]._id
+                        },
+                        process.env.MONGO_ATLAS_PW,
+                        {
+                            expiresIn: "4h"
+                        }
+                    );
                     return res.status(200).json({
-                        message: 'Auth successful'
+                        message: 'Auth successful',
+                        token: token
                     });
                 }
                 return res.status(401).json({
