8a8835803363dac77b8b470a465d8f254d00a9f6
Róbert Székely <szekelyrobert1995@gmail.com>
2019-02-03 16:07:05 +0200

    Add mongoose

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const bookRoutes = require('./api/routes/books');
+
+mongoose.connect("mongodb+srv://robertszekely:" + process.env.MONGO_ATLAS_PW + '@node-rest-books-omjs7.mongodb.net/test?retryWrites=true',
+    { useNewUrlParser: true }
+);
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
