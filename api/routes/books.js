0dac6af65f6db431550d678cbff063a7dfc920f4
Róbert Székely <szekelyrobert1995@gmail.com>
2019-02-03 16:54:24 +0200

    Implement update and delete, refactor get

--- api/routes/books.js
+++ api/routes/books.js
@@ -7,9 +7,15 @@ const Book = require('../models/book');
 router.get('/', (req, res, next) => {
     Book.find()
         .exec()
-        .then(docs => {
-            console.log(docs);
-            res.status(200).json(docs);
+        .then(books => {
+            console.log(books);
+            if (books.length >= 0) {
+                res.status(200).json(books);
+            } else {
+                res.status(404).json({
+                    message: 'No entries found'
+                })
+            }
         })
         .catch(err => {
             console.log(err);
@@ -30,7 +36,7 @@ router.post('/', (req, res, next) => {
         .then(result => {
             console.log(result)
             res.status(200).json({
-                message: 'Handling POST requests to /books',
+                message: 'Book was added successfully',
                 createdBook: book
             });
         })
@@ -46,10 +52,10 @@ router.get("/:bookId", (req, res, next) => {
     const id = req.params.bookId;
     Book.findById(id)
         .exec()
-        .then(doc => {
-            console.log(doc);
-            if (doc) {
-                res.status(200).json(doc);
+        .then(book => {
+            console.log(book);
+            if (book) {
+                res.status(200).json(book);
             } else {
                 res.status(404).json({ message: 'No valid entry found for provided id' })
             }
@@ -61,15 +67,35 @@ router.get("/:bookId", (req, res, next) => {
 });
 
 router.patch('/:bookId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Updated book'
+    const id = req.params.bookId
+    const updateData = req.body;
+    Book.update({ _id: id }, updateData)
+    .exec()
+    .then(result => {
+        console.log(result);
+        res.status(201).json(result);
     })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
 });
 
 router.delete('/:bookId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Deleted book'
-    })
+    const id = req.params.bookId
+    Book.remove({ _id: id })
+        .exec()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 module.exports = router;
