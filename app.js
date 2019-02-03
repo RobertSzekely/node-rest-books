0497f668a4578c91525176a7b6d6b5cc9b0c72af
Róbert Székely <szekelyrobert1995@gmail.com>
2019-02-03 13:35:11 +0200

    Add cors

--- app.js
+++ app.js
@@ -6,8 +6,18 @@ const bodyParser = require('body-parser');
 const bookRoutes = require('./api/routes/books');
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        req.header('Access-Control-Allow-Method', 'GET, PUT, POST, PATCH, DELETE');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 //Routes which handle requests
 app.use('/books', bookRoutes);
