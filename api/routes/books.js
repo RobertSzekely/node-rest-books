ca5c8901fa858b1a22d50c106b31b9471d9e4457
Róbert Székely <szekelyrobert1995@gmail.com>
2019-02-03 16:07:38 +0200

    Add book model and implement get from db

--- api/routes/books.js
+++ api/routes/books.js
@@ -1,47 +1,75 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Book = require('../models/book');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET requests to /books'
-    });
+    Book.find()
+        .exec()
+        .then(docs => {
+            console.log(docs);
+            res.status(200).json(docs);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.post('/', (req, res, next) => {
-    const book = {
+    const book = new Book({
+        _id: new mongoose.Types.ObjectId(),
         title: req.body.title,
         author: req.body.author
-    }
-    res.status(200).json({
-        message: 'Handling POST requests to /books',
-        createdBook: book
     });
+    book
+        .save()
+        .then(result => {
+            console.log(result)
+            res.status(200).json({
+                message: 'Handling POST requests to /books',
+                createdBook: book
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        });
 });
 
-router.get('/:bookId', (req, res, next) => {
+router.get("/:bookId", (req, res, next) => {
     const id = req.params.bookId;
-    if (id === 'special') {
-        res.status(200).json({
-            message: 'You discovered the special ID',
-            id: id
+    Book.findById(id)
+        .exec()
+        .then(doc => {
+            console.log(doc);
+            if (doc) {
+                res.status(200).json(doc);
+            } else {
+                res.status(404).json({ message: 'No valid entry found for provided id' })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: err });
         });
-    } else {
-        res.status(200).json({
-            message: 'You passed an ID'
-        });
-    }
 });
 
 router.patch('/:bookId', (req, res, next) => {
-   res.status(200).json({
-       message: 'Updated book'
-   })
+    res.status(200).json({
+        message: 'Updated book'
+    })
 });
 
 router.delete('/:bookId', (req, res, next) => {
     res.status(200).json({
         message: 'Deleted book'
     })
- });
+});
 
 module.exports = router;
